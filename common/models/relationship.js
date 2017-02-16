81553c39187f07598cc8a6ed01b87d4564da18ed
Gabriel Sabillon <gaboelnuevo@gmail.com>
2017-02-16 12:21:58 -0600

    operations hooks

--- common/models/relationship.js
+++ common/models/relationship.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Relationship) {
-
+  Relationship.observe('before save', function(ctx, next) {
+    var currentUserId = ctx.req.accessToken && ctx.req.accessToken.userId;
+    if (ctx.instance) {
+      ctx.instance.updatedAt = new Date();
+      ctx.instance.actionUserId = currentUserId;
+    } else {
+      ctx.data.updatedAt = new Date();
+      ctx.data.actionUserId = currentUserId;
+    }
+    next();
+  });
 };
