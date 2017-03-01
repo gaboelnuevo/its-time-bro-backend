fdceff28064b39348f566827663f10ed29983c5f
Gabriel Sabillon <gaboelnuevo@gmail.com>
2017-03-01 13:54:38 -0600

    validate relationships

--- common/models/relationship.js
+++ common/models/relationship.js
@@ -1,16 +1,32 @@
 'use strict';
 
 module.exports = function(Relationship) {
+  Relationship.validate(
+    'relationship',
+    relationshipValidator,
+    {message: 'Invalid relationship'}
+  );
+
+  function relationshipValidator(err) {
+    if (this.secondUserId > this.firstUserId) {
+      err();
+    }
+  }
+
   Relationship.observe('before save', function(ctx, next) {
     var currentContext = LoopBackContext.getCurrentContext();
     var currentUser = currentContext && currentContext.get('currentUser');
 
     if (ctx.instance) {
+      if (currentUser) {
+        ctx.instance.actionUserId = currentUser.id;
+      }
       ctx.instance.updatedAt = new Date();
-      ctx.instance.actionUserId = currentUser.id;
     } else {
+      if (currentUser) {
+        ctx.data.actionUserId = currentUser.id;
+      }
       ctx.data.updatedAt = new Date();
-      ctx.instance.actionUserId = currentUser.id;
     }
     next();
   });
