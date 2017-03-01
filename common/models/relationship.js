42b207284a1ef5c1191317d3f00c8ebcc144acb4
Gabriel Sabillon <gaboelnuevo@gmail.com>
2017-03-01 13:46:01 -0600

    relationship action user before save

--- common/models/relationship.js
+++ common/models/relationship.js
@@ -2,10 +2,15 @@
 
 module.exports = function(Relationship) {
   Relationship.observe('before save', function(ctx, next) {
+    var currentContext = LoopBackContext.getCurrentContext();
+    var currentUser = currentContext && currentContext.get('currentUser');
+
     if (ctx.instance) {
       ctx.instance.updatedAt = new Date();
+      ctx.instance.actionUserId = currentUser.id;
     } else {
       ctx.data.updatedAt = new Date();
+      ctx.instance.actionUserId = currentUser.id;
     }
     next();
   });
