9aaf76996c8d31e9e3830ffaab491b4c3376939b
Gabriel Sabillon <gaboelnuevo@gmail.com>
2017-05-18 18:07:05 -0600

    update datasource config

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -4,7 +4,6 @@ var mongodbUri = require('mongodb-uri');
 
 var URI = process.env.MONGODB_URI;
 var uriObject = URI ? mongodbUri.parse(URI) : null;
-
 var config = {};
 
 if (uriObject && uriObject.hosts.length >= 1) {
@@ -14,6 +13,8 @@ if (uriObject && uriObject.hosts.length >= 1) {
     host: uriObject.hosts[0].host,
     port: uriObject.hosts[0].port,
     database: uriObject.database,
+    username: uriObject.username,
+    password: uriObject.password,
   };
 }
 
